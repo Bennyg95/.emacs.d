2a4810987cd6c7f263c87dc00a633ca07f4360e7
Bennyg95 <sanchezbenitog95@hotmail.com>
2015-10-13 22:20:48 -0700

    updated emacs.

--- init.el
+++ init.el
@@ -46,6 +46,27 @@
       (quote (read-only t point-entered minibuffer-avoid-prompt
 			face minibuffer-prompt)))
 
+;; Transparent emacs
+ ;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
+ (set-frame-parameter (selected-frame) 'alpha '(85 50))
+ (add-to-list 'default-frame-alist '(alpha 85 50))
+
+ (eval-when-compile (require 'cl))
+ (defun toggle-transparency ()
+   (interactive)
+   (if (/=
+        (cadr (frame-parameter nil 'alpha))
+        100)
+       (set-frame-parameter nil 'alpha '(100 100))
+     (set-frame-parameter nil 'alpha '(85 50))))
+ (global-set-key (kbd "C-c t") 'toggle-transparency)
+
+ ;; Set transparency of emacs
+ (defun transparency (value)
+   "Sets the transparency of the frame window. 0=transparent/100=opaque"
+   (interactive "nTransparency Value 0 - 100 opaque:")
+   (set-frame-parameter (selected-frame) 'alpha value))
+
 ;;; yasnippet
 ;;; should be loaded before auto complete so that they can work together
 (require 'yasnippet)
